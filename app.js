233c6be3961079a9bcfd5d42f90f84ae93359211
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-02 13:59:55 -0400

    create add command using to add a new note

--- app.js
+++ app.js
@@ -2,4 +2,17 @@ const yargs = require('yargs')
 const getNotes = require('./notes.js')
 const chalk = require('chalk')
 
-console.log(chalk.green('success!!'))
+yargs.version('1.1.0')
+
+// Create add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function () {
+        console.log('Adding a new note!')
+    }
+})
+
+// Create remove command
+
+console.log(yargs.argv)
