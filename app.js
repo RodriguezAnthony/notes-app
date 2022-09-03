fa63b43ee0f540deb782f63667d3bb08ff2200c6
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-03 14:15:03 -0400

    add new notes using file system module to create notes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const yargs = require('yargs')
-const getNotes = require('./notes.js')
+const notes = require('./notes.js')
 const chalk = require('chalk')
+const { argv } = require('yargs')
 
 yargs.version('1.1.0')
 
@@ -8,8 +9,20 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Add a new note',
-    handler: function () {
-        console.log('Adding a new note!')
+    builder: {
+        title: {
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv) {
+        notes.addNotes(argv.title, argv.body)
     }
 })
 
@@ -40,4 +53,4 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
--- notes.js
+++ notes.js
@@ -1,6 +1,39 @@
+const fs = require('fs')
 
 const getNotes = () =>{
     return 'Your notes...'
 }
 
-module.exports = getNotes
+const addNote = (title, body) =>{
+    const notes = loadNotes()
+
+    notes.push({
+        title: title,
+        body: body
+    })
+
+    saveNotes(notes)
+}
+
+const saveNotes = (notes) => {
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJSON)
+}
+
+const loadNotes = () => {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e) {
+        return []
+    }
+
+
+
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNotes: addNote
+}
