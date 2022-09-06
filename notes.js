021ec53955cb2e0413d299afb5481b17e4159548
RodriguezAnthony <rodriguezanthony2918@gmail.com>
2022-09-05 21:31:49 -0400

    refactor to use arrow functions

--- notes.js
+++ notes.js
@@ -1,18 +1,26 @@
 const fs = require('fs')
+const chalk = require('chalk')
 
 const getNotes = () =>{
     return 'Your notes...'
 }
 
-const addNote = (title, body) =>{
+const addNote = (title, body) => {
     const notes = loadNotes()
+    const duplicateNotes = notes.filter((note) => note.title === title)
 
-    notes.push({
-        title: title,
-        body: body
-    })
+    if(duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log(chalk.green.inverse('New note added'))
+    } else {
+        console.log(chalk.red.inverse('Note title taken'))
+    }
 
-    saveNotes(notes)
+
 }
 
 const saveNotes = (notes) => {
@@ -28,12 +36,24 @@ const loadNotes = () => {
     } catch (e) {
         return []
     }
+}
 
+const removeNote = (title) => {
+    const notes = loadNotes()
+    const newNotes = notes.filter(note => note.title !== title)
 
+    if(notes.length !== newNotes.length){
+        saveNotes(newNotes)
+        console.log(chalk.green.inverse('Note Removed!'))
+    } else{
+        console.log(chalk.red.inverse('No note found!'))
+    }
 
+    
 }
 
 module.exports = {
     getNotes: getNotes,
-    addNotes: addNote
+    addNotes: addNote,
+    removeNote: removeNote
 }
